3c3b2f6669088d9e8246e8af9240d05f26299304
lennykossyk <lenny.kossyk@gmail.com>
2026-06-28 23:16:00 +0200

    06-28: Fix ORKA startup arm script path

--- orka-control/start_orka.sh
+++ orka-control/start_orka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ORKA_ARM_SCRIPT=/home/orka/ORKA-Code/lerobot/follower_server_webstatus.py
+export ORKA_ARM_SCRIPT=/home/orka/ORKA-Code/lerobot/follower_server.py
 export ORKA_ARM_PYTHON=/home/orka/ORKA-Code/lerobot/.venv/bin/python
 export ORKA_ARM_WORKDIR=/home/orka/ORKA-Code/lerobot
 export ORKA_ARM_PORT=/dev/ttyACM0
